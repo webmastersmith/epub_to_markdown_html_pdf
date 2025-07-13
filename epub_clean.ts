80d717aa64f8320152cf0c93390f08138369da50
webmastersmith <webmastersmith37@gmail.com>
2025-07-13 13:34:14 -0500

    epub to epub, modify text only

--- epub_clean.ts
+++ epub_clean.ts
@@ -1,4 +1,5 @@
 // node --experimental-strip-types .\epub_clean.ts
+// npm run build
 import fs from 'node:fs';
 import path from 'node:path';
 // import util from 'node:util';
@@ -8,9 +9,9 @@ import extract from 'extract-zip';
 import swearWords from './swear-words.json' with { type: 'json' };
 
 
-// 1. Epub files are archived files separated by into 'Text' dir, 'Styles','Images'...
+// 1. Epub files are archived(zip) files separated by into 'Text', 'Styles', 'Images'...
 // 2. To edit text, you can extract and map 'Text' directory files.
-// 3. Archive back to epub.
+// 3. Archive(zip) back to epub.
 
 (async () => {
   // scan for epub files.
@@ -23,6 +24,7 @@ import swearWords from './swear-words.json' with { type: 'json' };
     await cleanEpub(epubName);
   }
 
+  // Each Epub is extracted, cleaned, archived.
   async function cleanEpub(epubName: string) {
     const epubPath = path.join(epubDir, epubName);
     const parsed = path.parse(epubPath);
@@ -33,26 +35,31 @@ import swearWords from './swear-words.json' with { type: 'json' };
     const textPaths = epub.flow.map((meta) => path.join(epubBuildPath, meta?.href || ''));
     // console.log(textPaths);
     
-    
     // ALWAYS create dir with file name.
     if (fs.existsSync(epubBuildPath)) fs.rmSync(epubBuildPath, { recursive: true, force: true }); // remove
     fs.mkdirSync(epubBuildPath, { recursive: true });
-
+    
     // Extract Epub
     await extract(epubPath, { dir: epubBuildPath })
     console.log(`\n${parsed.name} was extracted.`);
     
-    // Read Text Files and removeSwearWords.
-    for (const textPath of textPaths) {
-      const htm = removeSwearWords(fs.readFileSync(textPath, 'utf-8'));
-      fs.writeFileSync(textPath, htm);
+    try {
+      // Read Text Files and removeSwearWords.
+      for (const textPath of textPaths) {
+        const htm = removeSwearWords(fs.readFileSync(textPath, 'utf-8'));
+        fs.writeFileSync(textPath, htm);
+      }
+      
+      // Archive Epub
+      const zip = new AdmZip();
+      zip.addLocalFolder(epubBuildPath);
+      zip.writeZip(path.join(epubBuildPath, `${parsed.name}-clean.epub`))
+      console.log(`${parsed.name} was archived.\n\n`);
+      
+    } catch (error) {
+      // error log and continue
+      console.log(error);      
     }
-    
-    // Archive Epub
-    const zip = new AdmZip();
-    zip.addLocalFolder(epubBuildPath);
-    zip.writeZip(path.join(epubBuildPath, `${parsed.name}-clean.epub`))
-    console.log(`${parsed.name} was archived.\n\n`);
   }
 
   function removeSwearWords(text: string) {
